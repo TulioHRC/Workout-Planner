a5f9af647f72be042017a07661f2b36c42632463
Grove <grove1411@gmail.com>
2022-01-06 18:50:54 -0200

    Basic popup exercises creation

--- src/components/popup-features/WorkoutCreation.js
+++ src/components/popup-features/WorkoutCreation.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 
 class WorkoutCreation extends Component {
+
     constructor(props) {
         super(props)
     
         this.state = {
-            serie: [] // Here will be added all the exercises series
+            serie: [], // Here will be added all the exercises series
+            count: 0
         }
     }
-    
+
+    createWorkout = () => {
+        this.setState((prevState) => ({ // Adding the new exercise to the actual state
+            serie: [...prevState.serie, {
+                name: 'Exercise',
+                position: prevState.count+1
+            }],
+            count: prevState.count+1
+        }))
+    }
 
     render() {
+        const { serie } = this.state
+
         return (
             <div className='popup-workouts'>
-                <button className='popup-btn-add'>+</button>
+                {
+                    
+                    serie.map((workout) => {
+                        // put the exercise component here
+                    })
+                    
+                }
+                <button onClick={this.createWorkout} className='popup-btn-add'>+</button>
             </div>
         )
-        // popup-workouts can be grid displayed
+        // There will be 8 workouts displayed at the same time
     }
 }
 
